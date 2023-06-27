9a18bb787157fb0c95023b5282dfacae0428b3aa
Openclassrooms <openclassrooms@macbook-pro-de-mehdi.home>
2023-06-27 20:29:54 +0200

    projet terminé

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,54 @@
+// Index du slide actuel
+let slideIndex = 0;
+
+// Chemin des slides
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  './assets/images/slideshow/slide1.jpg',
+  './assets/images/slideshow/slide2.jpg',
+  './assets/images/slideshow/slide3.jpg',
+  './assets/images/slideshow/slide4.png'
+];
+
+// Afficher le premier slide à l'ouverture de la page
+window.onload = function() {
+  showSlide(slideIndex);
+};
+
+// Afficher le slide correspondant à l'index
+function showSlide(n) {
+  const img = document.querySelector('#banner .banner-img');
+  img.src = slides[n];
+  updateDots(n);
+}
+
+// Mettre à jour les "dots" en fonction du slide actuel
+function updateDots(n) {
+  const dots = document.querySelectorAll('.dot');
+  dots.forEach((dot, index) => {
+    dot.classList.remove('dot_selected');
+    if (index === n) {
+      dot.classList.add('dot_selected');
+    }
+  });
+}
+
+// Changer de slide
+function changeSlide(n) {
+  slideIndex += n;
+  if (slideIndex > slides.length - 1) {
+    slideIndex = 0;
+  } else if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  showSlide(slideIndex);
+}
+
+// Listener pour le bouton précédent
+document.querySelector('#banner .arrow_left').addEventListener('click', function() {
+  changeSlide(-1);
+});
+
+// Listener pour le bouton suivant
+document.querySelector('#banner .arrow_right').addEventListener('click', function() {
+  changeSlide(1);
+});
